118f1b4731c251c12403c50a4e1fc593a5dda1ec
duscio <duscio@olomedia.it>
2025-04-11 15:13:35 +0200

    dinamicizzazione navbar-navigazione

--- src/app/components/navbar-navigazione/navbar-navigazione.component.ts
+++ src/app/components/navbar-navigazione/navbar-navigazione.component.ts
@@ -1,12 +1,38 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-navigazione',
   standalone: true,
-  imports: [],
+  imports: [NgFor, RouterLink],
   templateUrl: './navbar-navigazione.component.html',
   styleUrl: './navbar-navigazione.component.sass'
 })
 export class NavbarNavigazioneComponent {
+  constructor(){
+    // console.log(this.sezioni);
+  }
+  
+  sezioni=[
+    { title: 'Ticket',          link: '/ticket' },
+    { title: 'Pazienti',        link: '/pazienti' },
+    { title: 'Prenotazioni',    link: '/prenotazione' },
+    { title: 'Agende',          link: '/agende' },
+    { title: 'Flussi',          link: '/flussi' },
+    { title: 'Ricerca',         link: '/ricerca' },
+    { title: 'Accettazioni',    link: '/accettazione' },
+    { title: 'Report',          link: '/report' },
+    { title: 'Tabelle',         link: '/tabelle' },
+    { title: 'Utenti',          link: '/utenti' },
+    { title: 'Unità Operative', link: '/unita_operative' }
+  ]
 
+  reparti=[
+    { title: 'Radiologia',  link: '/radiologia' },
+    { title: 'Cardiologia', link: '/cardiologia' },
+    { title: 'Neurologia',  link: '/neurologia' },
+    { title: 'Ortopedia',   link: '/ortopedia' },
+    { title: 'Oftalmologia',link: '/oftalmologia' },
+  ]
 }
