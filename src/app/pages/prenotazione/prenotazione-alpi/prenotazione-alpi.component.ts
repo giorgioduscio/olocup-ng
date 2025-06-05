38916c806f596c1f01e378deccf98bdf80ce12db
duscio <duscio@olomedia.it>
2025-06-05 13:44:37 +0200

    import sass e html statico

--- src/app/pages/prenotazione/prenotazione-alpi/prenotazione-alpi.component.ts
+++ src/app/pages/prenotazione/prenotazione-alpi/prenotazione-alpi.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { ModalComponent } from "../../../shared/modal/modal.component";
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-prenotazione-alpi',
   standalone: true,
-  imports: [],
+  imports: [ModalComponent, NgFor, NgIf],
   templateUrl: './prenotazione-alpi.component.html',
   styleUrl: './prenotazione-alpi.component.sass'
 })
 export class PrenotazioneAlpiComponent {
-
+  tabsDatas =[
+    {key:'prestazione', title:'Prestazione',icon:'it-note',     status:'active'},
+    {key:'datetime',    title:'Data e Ora', icon:'it-calendar', status:'disabled'},
+    {key:'paziente',    title:'Paziente',   icon:'it-user',     status:'disabled'},
+    {key:'confirm',     title:'Conferma',   icon:'it-check',    status:'disabled'},
+  ]
 }
